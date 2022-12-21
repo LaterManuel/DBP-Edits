11579109495c1d40d272b16cc19b754c7fa5436d
Manuel Cáceres <sixto.caceres@ucsp.edu.pe>
2022-12-20 21:51:47 -0500

    AhoraSi - Main

---
+++ AhoraSi/AhoraSi/AhoraSi/Page1.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AhoraSi
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            string english = "";
+            string leng = "";
+            string habil = "";
+            if (videojuegos.IsChecked)
+            {
+                habil += "- Programacion videojuegos" + Environment.NewLine;
+            }
+            if (office.IsChecked)
+            {
+                habil += "- Uso de Ofimatica de Office" + Environment.NewLine;
+            }
+            if (video.IsChecked)
+            {
+                habil += "- Experto en creacion de Videos" + Environment.NewLine;
+            }
+            if (musica.IsChecked)
+            {
+                habil += "- Musico de hobby" + Environment.NewLine;
+            }
+            if (editor.IsChecked)
+            {
+                habil += "- Editor de videos 4k" + Environment.NewLine;
+            }
+            if (Python.IsChecked)
+            {
+                leng += " Python ";
+            }
+            if (Java.IsChecked)
+            {
+                leng += " Java ";
+            }
+            if (Html.IsChecked)
+            {
+                leng += " Html ";
+            }
+            if (C.IsChecked)
+            {
+                leng += " C ";
+            }
+            if (basico.IsChecked)
+            {
+                english = basico.Value.ToString();
+            }
+            if (intermedio.IsChecked)
+            {
+                english = intermedio.Value.ToString();
+            }
+            if (avanzado.IsChecked)
+            {
+                english = avanzado.Value.ToString();
+            }
+            if (fluido.IsChecked)
+            {
+                english = fluido.Value.ToString();
+            }
+            string nombre = EntryNombre.Text;
+            string nacimiento = EntryNacimiento.Date.ToString("d");
+            string ocupacion = EntryOcupacion.Text;
+            string correo = EntryCorreo.Text;
+            string nacionalidad = (string)EntryNacionalidad.ItemsSource[EntryNacionalidad.SelectedIndex];
+            string ingles = english;
+            string lenguaje = leng;
+            string habilidades = habil;
+            string perfil = EntryPerfil.Text;
+            string aptitudes = EntryApt.Text;
+            string experiencia = EntryExp.Text;
+            string formacion = EntryForm.Text;
+            await Application.Current.MainPage.Navigation.PushAsync(new Page2(nombre, ocupacion, correo, nacimiento, nacionalidad, perfil, ingles, lenguaje, habilidades, aptitudes, experiencia, formacion));
+        }
+    }
+}
---
+++ AhoraSi/AhoraSi/AhoraSi/Page2.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AhoraSi
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2(string nombre, string ocupacion, string correo, string nacimiento, string nacionalidad, string perfil, string ingles, string lenguaje, string habilidades, string aptitudes, string experiencia, string formacion)
+        {
+            InitializeComponent();
+            nombr.Text = nombre;
+            ocp.Text = ocupacion;
+            nac.Text = nacimiento;
+            cor.Text = "Correo: " + correo + Environment.NewLine + Environment.NewLine + "Nacionalidad: " + nacionalidad;
+            perf.Text = perfil;
+            ing.Text = "Ingles: " + ingles;
+            leng.Text = "Leng. de program: " + lenguaje + Environment.NewLine + "Aptitudes: " + aptitudes;
+            hab.Text = habilidades;
+            exp.Text = experiencia;
+            form.Text = formacion;
+        }
+    }
+}
